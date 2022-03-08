fa0b81e0d5fa55bf966805df510e81d98df2845e
DarkedgeTH <darkedgeth@gmail.com>
2022-03-08 16:26:50 -0500

    set up project and structure

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,6 +4,20 @@ const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+        <div id='error'>Error: No error here</div>
+        <div id='todos'>
+          <h2>Todos:</h2>
+          <div>Walk the dog</div>
+          <div>Learn React</div>
+        </div>
+        <form id='todoForm'>
+          <input type='text' placeholder='Type todo' />
+          <input type='submit'></input>
+          <button>Hide Completed</button>
+        </form>
+      </div>
+    )
   }
 }
