c6c10423dce0c9a61cc61e81adfad0508cee2526
DarkedgeTH <darkedgeth@gmail.com>
2022-03-08 16:32:01 -0500

    added state and imported axios to get URL

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+  }
+
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div>
